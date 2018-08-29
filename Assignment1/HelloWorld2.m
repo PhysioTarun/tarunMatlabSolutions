00b5a4d5717a962a03bbdb5da449c70474dc5bc3
PhysioTarun <PhysioTarun@users.noreply.github.com>
2018-08-28 23:23:35 -0400

    complete

---
+++ Assignment1/HelloWorld2.m
@@ -0,0 +1,4 @@
+fprintf ('\n -----------------------')
+fprintf ('\n Hello World \n')
+msg = input ('Enter your message to the world: ','s');
+fprintf ('Your message "%s" is sent to everyone! \n', msg)
