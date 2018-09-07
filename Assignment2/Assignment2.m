62ddc80967d922d79a124a94a4779157552b292e
PhysioTarun <PhysioTarun@users.noreply.github.com>
2018-09-07 13:54:46 -0400

    init commit

---
+++ Assignment2/Assignment2.m
@@ -0,0 +1,19 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Assignment 2: Chapter 1, 2 & 3 Exercises
+%
+% Submitted by: Tarun K Singh
+%
+% Due: September 13, 2018
+%
+% Instructions: 
+% Create a new m-file in MATLAB and save your answers in it. At the top of
+% your file, you should have a comment section with your name, the 
+% due date, and a brief description of what is contained in the file.
+% This will be the standard format going forward for your assignments. 
+% Exercises can be found at the ends of Chapters 1, 2 and 3 in your
+% textbook.
+% If there are not comments please write your answer with correct 
+% matlab syntax. 
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
